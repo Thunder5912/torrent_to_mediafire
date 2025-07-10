01ca048d5f87514fac7e412a646d2b08b25ade59
Thunder5912 <122851882+Thunder5912@users.noreply.github.com>
2025-07-10 20:27:29 +0530

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MEDIAFIRE_EMAIL = os.getenv("MEDIAFIRE_EMAIL")
-MEDIAFIRE_PASSWORD = os.getenv("MEDIAFIRE_PASSWORD")
+BOT_TOKEN = os.getenv("7774532872:AAGuCHgRqvDuW2mknvUTeG4oYL3j6Kx0eiA")
+MEDIAFIRE_EMAIL = os.getenv("madithya5912@gmail.com")
+MEDIAFIRE_PASSWORD = os.getenv("Thunder@5912")
